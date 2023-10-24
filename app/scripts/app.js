7dbebb04bb24cada6945bb3f778e05479409628f
Claret I <clareti@TAG.DC>
2023-10-24 13:07:25 +0530

    logged in name displayed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,24 @@
-var client;
+let client;
 
 init();
 
 async function init() {
   client = await app.initialized();
-  client.events.on('app.activated', renderText);
+  client.events.on("app.activated", renderText);
 }
 
 async function renderText() {
-  const textElement = document.getElementById('apptext');
-  const contactData = await client.data.get('contact');
-  const {
-    contact: { name }
-  } = contactData;
-
-  textElement.innerHTML = `Ticket is created by ${name}`;
+  client.events.on("ticket.forwardClick", forwardClick);
 }
+
+const spotlight = document.querySelector(".spotlight");
+
+//Open Forward editor dialog box
+const forwardClick = async (event) => {
+  let loggedInUser = await client.data.get("loggedInUser");
+  let event_data = await event.helper.getData();
+  const row = `<fw-label value="${loggedInUser?.loggedInUser?.contact?.name}" color="green"></fw-label>`;
+  spotlight.insertAdjacentHTML("afterend", row);
+  console.log(event, "----event");
+  console.log(loggedInUser?.loggedInUser?.contact?.name, "---->logged in user");
+};
