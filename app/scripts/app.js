a8e7cc6f03c841763ebad65000bdb73172f68afc
Claret I <clareti@TAG.DC>
2023-11-02 12:05:29 +0530

    reply text editor - sender details added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,5 @@
 let client;
+// let username = "";
 
 init();
 
@@ -8,17 +9,33 @@ async function init() {
 }
 
 async function renderText() {
-  client.events.on("ticket.forwardClick", forwardClick);
+  client.events.on("ticket.replyClick", replyClick);
 }
 
 const spotlight = document.querySelector(".spotlight");
 
-//Open Forward editor dialog box
-const forwardClick = async (event) => {
+//Open Reply editor dialog box
+const replyClick = async (event) => {
   let loggedInUser = await client.data.get("loggedInUser");
   let event_data = await event.helper.getData();
   const row = `<fw-label value="${loggedInUser?.loggedInUser?.contact?.name}" color="green"></fw-label>`;
+  let username = loggedInUser?.loggedInUser?.contact?.name;
   spotlight.insertAdjacentHTML("afterend", row);
   console.log(event, "----event");
   console.log(loggedInUser?.loggedInUser?.contact?.name, "---->logged in user");
+  await client.interface
+    .trigger("setValue", {
+      id: "editor",
+      text: `<div class="fw-type-light">Regards</div>
+             <div class="fw-type-light">${username}</div>`,
+      replace: false,
+      position: "end",
+    })
+    .then(function (data) {
+      // data - success message
+      console.log(data, "--->data");
+    })
+    .catch(function (error) {
+      // error - error object
+    });
 };
